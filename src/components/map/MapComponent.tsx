826c911fdaeefd6864d881d4dcfd3c2509b733db
snaklc <senakilic3@gmail.com>
2021-12-08 11:19:53 +0300

    added basemaps

--- src/components/map/MapComponent.tsx
+++ src/components/map/MapComponent.tsx
@@ -1,28 +1,50 @@
+import './MapComponent.scss'
 import 'ol/ol.css';
 import Map from 'ol/Map';
-import OSM from 'ol/source/OSM';
-import TileLayer from 'ol/layer/Tile';
 import View from 'ol/View';
 import { useEffect } from 'react';
-import './MapComponent.scss'
-
+import TileWMS from 'ol/source/TileWMS';
+import { Tile as TileLayer } from 'ol/layer';
+import OSM from 'ol/source/OSM';
+import { add } from 'ol/coordinate';
 
 export const MapComponent = () => {
     useEffect(() => {
+
+        const map: Map = createMap();
+        addKampusBasemap(map);
+
+    }, [])
+
+    const createMap = () => {
         const map = new Map({
             layers: [
                 new TileLayer({
                     source: new OSM(),
                 }),
+
             ],
             target: 'map',
             view: new View({
-                center: [0, 0],
-                zoom: 2,
+                center: [28.96, 41.03],
+                zoom: 5,
+                projection: "EPSG:3857"
             }),
         });
-      
-    }, [])
+        return map;
+    }
+
+    const addKampusBasemap = (map: Map) => {
+        const basemap = new TileLayer({
+            source: new TileWMS({
+                url: 'https://kampus.ankageo.com/geoserver/kampus/wms',
+                params: { 'LAYERS': 'kampus:altlik', 'TILED': true },
+                serverType: 'geoserver',
+            }),
+        });
+
+        map.addLayer(basemap);
+    }
 
     return (
         <div>
